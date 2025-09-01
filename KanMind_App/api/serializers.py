4d408aa31362f3b45e2851309f81fb66d9eb8838
VincentGörner <161941709+A1exi0sD4rc@users.noreply.github.com>
2025-09-01 19:32:41 +0200

    update_serilizers

--- KanMind_App/api/serializers.py
+++ KanMind_App/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from KanMind_App.models import User, Board, Task, Comment
+from KanMind_App.models import User, Member, Board, Task, Column, Comment
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -8,19 +8,46 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ['user', 'joined_date']
 
 class BoardSerializer(serializers.ModelSerializer):
-    member_count = serializers.SerializerMethodField()
+    member_count = serializers.SerializerMethodField(read_only=True)
+    ticket_count = serializers.SerializerMethodField(read_only=True)
+    tasks_to_do_count = serializers.SerializerMethodField(read_only=True)
+    tasks_high_prio_count = serializers.SerializerMethodField(read_only=True)
+
+    members = UserSerializer(many=True, read_only=True)
+    member_ids = serializers.PrimaryKeyRelatedField(
+        queryset=Member.objects.all(),
+        many=True,
+        write_only=True,
+        source='members'
+    )
+    owner = UserSerializer(read_only=True)
 
     class Meta:
         model = Board
-        fields = ['id', 'title', 'member_count']
+        fields = ['id', 'title', 'member_count', 'ticket_count', 'tasks_to_do_count', 'tasks_high_prio_count', 'owner', 'members', 'member_ids', 'owner_id']
 
     def get_member_count(self, obj):
         return obj.members.count()
+    
+    def get_ticket_count(self, obj):
+        return obj.tasks.count()
+    
+    def get_tasks_to_do_count(self, obj):
+        return obj.columns.filter(type='TODO').count()
+    
+    def get_tasks_high_prio_count(self, obj):
+        tasks = obj.tasks.filter(priority=Task.PriorityChoices.high)
+        return tasks.count()
 
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = ['id', 'title', 'board', 'description', 'status', 'priority']
+
+class ColumSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Column
+        fields = ['title', 'assigned_tasks']
 
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
--- KanMind_App/models.py
+++ KanMind_App/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class Member(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    joined_date = models.DateField(auto_now_add=True)
+    joined_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.user
@@ -13,29 +13,54 @@ class Board(models.Model):
     title = models.CharField(max_length=50)
     members = models.ManyToManyField(Member, related_name="boards")
     owner = models.ForeignKey(Member, on_delete=models.CASCADE)
-    todo = models.JSONField(null=True, blank=True)
-    in_progress = models.JSONField(null=True, blank=True)
-    review = models.JSONField(null=True, blank=True)
-    done = models.JSONField(null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        is_new_board = self._state.adding
+
+        super().save(*args, **kwargs)
+
+        if is_new_board:
+            self.columns = [
+                ('TODO', 'To Do'),
+                ('IN_PROGRESS', 'In Progress'),
+                ('REVIEW', 'Review'),
+                ('DONE', 'Done'),
+            ]
+            for index, (column_type) in enumerate(columns):
+                Column.objects.create(board=self, type=column_type, order=index)
 
     def __str__(self):
         return self.title
+    
+class Column(models.Model):
+    board = models.ForeignKey(Board, on_delete=models.CASCADE, related_name="columns")
+    order = models.PositiveIntegerField(default=0)
+    type = models.CharField(max_length=20,
+        choices=[
+            ('TODO', 'To Do'),
+            ('IN_PROGRESS', 'In Progress'),
+            ('REVIEW', 'Review'),
+            ('DONE', 'Done'), 
+        ])
+
+    def __str__(self):
+        return f"{self.get_type_display()} ({self.board.title})"
 
 class Task(models.Model):
-    PRIOITY_CHOICES = [
-        ('low', 'Low'),
-        ('medium', 'Medium'),
-        ('high', 'High')
-    ]
+    class PriorityChoices(models.TextChoices):
+        low = 'Low'
+        medium = 'Medium'
+        high = 'High'
 
-    board = models.ForeignKey(Board, on_delete=models.CASCADE)
+    board = models.ForeignKey(Board, on_delete=models.CASCADE, related_name="tasks")
     title = models.CharField(max_length=100)
-    description = models.CharField(max_length=255, blank=True)
-    status = models.CharField() # muss geändert werden
-    priority = models.CharField(max_length=10, choices=PRIOITY_CHOICES, default='low')
+    description = models.CharField(max_length=255, blank=True, null=True)
+    status = models.ForeignKey(Column, on_delete=models.CASCADE)
+    priority = models.CharField(max_length=10, choices=PriorityChoices.choices, default='low')
     assignee_id = models.ManyToManyField(Member, related_name="tasks")
     reviewer_id = models.ForeignKey(Member, on_delete=models.SET_NULL, null=True, blank=True, related_name="reviewed_tasks")
-    due_date = models.DateField(editable=True)
+    due_date = models.DateField(editable=True, null=True, blank=True)
+    order = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return self.title
