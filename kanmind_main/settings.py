8faf06561fa36c4db80c28f1cdb6a0e488076bc1
VincentGörner <161941709+A1exi0sD4rc@users.noreply.github.com>
2025-09-27 14:26:50 +0200

    delete_comments

--- kanmind_main/settings.py
+++ kanmind_main/settings.py
@@ -62,11 +62,6 @@ CSRF_TRUSTED_ORIGINS = [
 
 CORS_ALLOW_ALL_ORIGINS = True
 
-# CORS_ALLOWED_ORIGINS = [
-#   'http://127.0.0.1:5500',
-#   'http://localhost:5500',
-# ]
-
 CORS_ALLOW_METHODS = [
   'GET',
   'POST',
